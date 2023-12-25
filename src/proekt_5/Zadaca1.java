a1ab93f06d4b929a025fb118f4aed7730dca75f1
EvaIlievska <ilievska_eva@yahoo.com>
2023-12-25 17:57:54 +0100

    proekt pet

---
+++ src/proekt_5/Zadaca1.java
@@ -0,0 +1,16 @@
+package proekt_5;
+
+public class Zadaca1 {
+
+	public static void main(String[] args) {
+		        int a = 5; 
+		        int b = 10; 
+		        int c = 3; 
+		        if (a <= 5 && b > (a - c)) {
+		            System.out.println("k = 1");
+		        } else {
+		            System.out.println("d = 0");
+		        }
+		    }
+
+	}
